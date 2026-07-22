06e30d103db1b81ead3642134fb52dc334cf96ce
ComputerChicken <gamegamemanage@gmail.com>
2026-07-21 18:58:40 -0600

    email!!

--- server/main.js
+++ server/main.js
@@ -7,14 +7,39 @@ const port = 21478;
 
 app.use(cors());
 
-app.use(express.json()); 
+app.use(express.json());
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'thedarraghbennett@gmail.com',
+    pass: 'oisrgbmluuwarmpv'
+  }
+});
 
 async function writeTextFile(name, content) {
     console.log("Recieved plan form from: " + name)
     try {
         await fs.writeFile(name + '.txt', content, 'utf8');
-        return true;
         console.log('File written successfully!');
+        console.log("Notifying myself...")
+        const mailOptions = {
+            from: '"Darragh Bennett" <thedarraghbennett@gmail.com>',
+            to: 'thedarraghbennett@gmail.com',
+            subject: 'Yo someone bought something',
+            text: 'Name:' + name + "\nContent: " + content
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log('Error occurred: ' + error.message);
+            }
+            console.log('Email sent successfully! Message ID: ' + info.messageId);
+        });
+        return true;
+
     } catch (err) {
         console.error('Error writing file:', err);
         return false;
